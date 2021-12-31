5b5af5214de1e219c25cff4c64b1579187f55b4d
Chad Riston Denaux <Riston@Chads-MacBook-Pro.local>
2021-12-31 14:34:07 -0500

    server 3

--- config/database.js
+++ config/database.js
@@ -16,6 +16,10 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variable": 'DATABASE_URL'
+    "use_env_variable": 'DATABASE_URL',
+    ssl = true,
+    dialectOptions = {
+      ssl: true,
+    },
   }
 }
