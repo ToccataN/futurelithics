cbe7697a0cb3f2fe52f17c74ca96a97e335ae3d2
Chad Riston Denaux <Riston@Chads-MacBook-Pro.local>
2022-02-25 13:33:17 -0500

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from  'react-redux';
-import { configureStore } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-import {Helmet} from "react-helmet";
-
-import MainLayout from './components/layouts/MainLayout'
+import MainLayout from "./components/layouts/MainLayout";
 import "./App.scss";
 
 function App() {
-  
   const { store, persistor } = configureStore();
 
   return (
-    <Provider store={store} >
+    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <div className="App mt-4 pt-4">
-            <Helmet>
-                <meta charSet="utf-8" />
-                <meta name="description" content="Future Lithics is a web & software consultancy founded by Chad R. Denaux." />
-                <title>Future Lithics</title>
-                <link rel="canonical" href="https://futurelithics.com" />
-            </Helmet>
             <MainLayout />
           </div>
         </BrowserRouter>
--- client/src/components/modals/ModalContainer.js
+++ client/src/components/modals/ModalContainer.js
@@ -1,60 +1,59 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Modal, ModalHeader, ModalBody} from 'reactstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
 
-import Login from './modalFragments/Login';
-import Register from './modalFragments/Register';
-import Alert from './modalFragments/Alert';
+import Login from "./modalFragments/Login";
+import Register from "./modalFragments/Register";
+import Alert from "./modalFragments/Alert";
 
 const ModalContainer = (props) => {
-  
   const { modalOpen, setModalOpen, type, response } = props;
 
   const toggle = () => {
-  	setModalOpen(!modalOpen);
-  }
+    setModalOpen(!modalOpen);
+  };
 
   const capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
-  }
+    if (typeof s !== "string") return "";
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
 
   const modalSwitch = (type) => {
-    switch(type){
-      case 'login':
-        return (<Login toggle={toggle} />);
-      case 'register':
-        return (<Register toggle={toggle} />);
-      case 'alert':
-        return (<Alert toggle={toggle} response={response}/>);
+    switch (type) {
+      case "login":
+        return <Login toggle={toggle} />;
+      case "register":
+        return <Register toggle={toggle} />;
+      case "alert":
+        return <Alert toggle={toggle} response={response} />;
       default:
-        return (<p>Not a Valid Modal Type</p>);
+        return <p>Not a Valid Modal Type</p>;
     }
-  }
+  };
 
   const headerText = () => {
-    if(type == 'alert' && response.title != undefined) {
+    if (type == "alert" && response.title != undefined) {
       return response.title;
     } else {
       return type;
     }
-  }
+  };
 
-	return (
-    <Modal isOpen={modalOpen} toggle={() => toggle()} style={{top: "20vh"}}>
-      <ModalHeader toggle={() => toggle()}>{ capitalize(headerText()) }</ModalHeader>
-      <ModalBody>
-        {modalSwitch(type)}           
-      </ModalBody>
+  return (
+    <Modal isOpen={modalOpen} toggle={() => toggle()} style={{ top: "20vh" }}>
+      <ModalHeader toggle={() => toggle()}>
+        {capitalize(headerText())}
+      </ModalHeader>
+      <ModalBody>{modalSwitch(type)}</ModalBody>
     </Modal>
-	);
-}
+  );
+};
 
 ModalContainer.propTypes = {
-	modalOpen: PropTypes.bool,
-	setModalOpen: PropTypes.func,
+  modalOpen: PropTypes.bool,
+  setModalOpen: PropTypes.func,
   type: PropTypes.string,
-  response: PropTypes.any
-}
+  response: PropTypes.any,
+};
 
 export default ModalContainer;
